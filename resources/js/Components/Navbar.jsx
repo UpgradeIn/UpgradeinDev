93ebacda1c0cb05b8a9a0bd51314458187929115
dimasardnt6 <dimasardnt6@gmail.com>
2025-02-22 00:38:57 +0700

    Add Hero section, About Section, Service Section, Why Choose Us Section

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -4,15 +4,37 @@ import { motion, AnimatePresence } from "framer-motion";
 
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
 
+    // Fungsi untuk toggle menu (mobile)
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
+    // Dengarkan event scroll
+    useEffect(() => {
+        const handleScroll = () => {
+            // Jika posisi scroll > 0, ubah state `scrolled` menjadi true
+            setScrolled(window.scrollY > 0);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        // Bersihkan event listener saat unmount
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="fixed top-0 z-10 bg-white w-full px-5 py-5 sm:px-10">
+        <nav
+            className={`
+                fixed top-0 z-20 w-full px-5 py-5 sm:px-10 transition-all duration-500
+                ${scrolled ? "bg-white shadow-md" : "bg-transparent"}
+            `}
+        >
             <div className="flex items-center justify-between">
-                <Link classID="flex items-center" href="/">
+                {/* Logo */}
+                <Link className="flex items-center" href="/">
                     <img
                         src="images/full-logo.png"
                         alt="Logo"
@@ -20,7 +42,7 @@ export default function Navbar() {
                     />
                 </Link>
 
-                {/* Hamburger Small Screen */}
+                {/* Hamburger (Small Screen) */}
                 <div className="flex sm:hidden">
                     <button onClick={toggleMenu}>
                         <svg
@@ -29,7 +51,7 @@ export default function Navbar() {
                             viewBox="0 0 24 24"
                             strokeWidth={1.5}
                             stroke="currentColor"
-                            className="size-6"
+                            className="w-6 h-6"
                         >
                             <path
                                 strokeLinecap="round"
@@ -39,9 +61,8 @@ export default function Navbar() {
                         </svg>
                     </button>
                 </div>
-                {/* End Hamburger Small Screen */}
 
-                {/* Menu Large Screen */}
+                {/* Menu (Large Screen) */}
                 <div className="hidden sm:block flex-1">
                     <ul className="flex justify-end space-x-10">
                         {["home", "about", "service", "project", "contact"].map(
@@ -49,83 +70,77 @@ export default function Navbar() {
                                 <li key={index}>
                                     <Link
                                         href={"/" + item}
-                                        className="text-gray-500 hover:text-gray-700"
+                                        className="font-medium text-gray-800 hover:text-[#09316E] capitalize"
                                     >
-                                        {item.charAt(0).toUpperCase() +
-                                            item.slice(1)}
+                                        {item}
                                     </Link>
                                 </li>
                             )
                         )}
                     </ul>
                 </div>
-                {/* End Menu Large Screen */}
-
-                {/* Menu Small Screen */}
-                <AnimatePresence>
-                    {isMenuOpen && (
-                        <motion.div
-                            initial={{ opacity: 0, x: 100 }}
-                            animate={{ opacity: 1, x: 0 }}
-                            exit={{ opacity: 0, x: 100 }}
-                            transition={{ duration: 0.3 }}
-                            className="absolute top-0 left-0 right-0 h-screen bg-white z-10 sm:hidden"
-                        >
-                            {/* button close */}
-                            <div className="relative px-5 py-5">
-                                <Link
-                                    classID="absolute top-5 px-5 flex items-center"
-                                    href="/"
-                                >
-                                    <img
-                                        src="images/full-logo.png"
-                                        alt="Logo"
-                                        className="h-8"
-                                    />
-                                </Link>
-                                <div className="absolute top-5 right-0 px-5">
-                                    <button onClick={toggleMenu}>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            strokeWidth={1.5}
-                                            stroke="currentColor"
-                                            className="size-6"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                d="M6 18 18 6M6 6l12 12"
-                                            />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                            <ul className="flex flex-col h-full justify-center items-center space-y-10">
-                                {[
-                                    "home",
-                                    "about",
-                                    "service",
-                                    "project",
-                                    "contact",
-                                ].map((item, index) => (
-                                    <li key={index}>
-                                        <Link
-                                            href={"/" + item}
-                                            className="text-xl text-gray-500 hover:text-gray-700"
-                                        >
-                                            {item.charAt(0).toUpperCase() +
-                                                item.slice(1)}
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </motion.div>
-                    )}
-                </AnimatePresence>
-                {/* End Menu Small Screen */}
             </div>
+
+            {/* Menu (Small Screen, Overlay) */}
+            <AnimatePresence>
+                {isMenuOpen && (
+                    <motion.div
+                        initial={{ opacity: 0, x: 100 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: 100 }}
+                        transition={{ duration: 0.5 }}
+                        className="absolute top-0 left-0 right-0 h-screen bg-white z-10 sm:hidden"
+                    >
+                        {/* Header (Logo & Close Button) */}
+                        <div className="flex items-center justify-between p-5">
+                            <Link href="/" className="flex items-center">
+                                <img
+                                    src="images/full-logo.png"
+                                    alt="Logo"
+                                    className="h-8"
+                                />
+                            </Link>
+                            <button onClick={toggleMenu}>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M6 18 18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+
+                        {/* Menu Items */}
+                        <ul className="flex flex-col h-full justify-center items-center space-y-10">
+                            {[
+                                "home",
+                                "about",
+                                "service",
+                                "project",
+                                "contact",
+                            ].map((item, index) => (
+                                <li key={index}>
+                                    <Link
+                                        href={"/" + item}
+                                        className="text-xl font-medium text-gray-800 hover:text-[#09316E] capitalize"
+                                        onClick={toggleMenu}
+                                    >
+                                        {item}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </nav>
     );
 }
---
+++ resources/js/Components/Slider.jsx
@@ -0,0 +1,54 @@
+import { motion } from "framer-motion";
+
+const images = [
+    "/images/hero-slider/image-1.png",
+    "/images/hero-slider/image-2.png",
+    "/images/hero-slider/image-3.png",
+    "/images/hero-slider/image-2.png",
+    "/images/hero-slider/image-1.png",
+];
+
+const Slider = () => {
+    return (
+        <div className="overflow-hidden w-full relative h-[350px] md:h-[400px] lg:h-[500px]">
+            <div className="absolute top-0 left-0 w-full h-[50px] bg-gradient-to-b from-white to-transparent z-10"></div>
+            <div className="absolute bottom-0 left-0 w-full h-[50px] bg-gradient-to-t from-white to-transparent z-10"></div>
+
+            <div className="grid grid-cols-3 gap-x-5 gap-y-5 w-full h-full sm:p-3 sm:grid-cols-4 relative">
+                {[...Array(4)].map((_, colIndex) => (
+                    <motion.div
+                        key={colIndex}
+                        className="flex flex-col space-y-5"
+                        animate={{
+                            y: colIndex % 2 === 0 ? [0, -500] : [-500, 0],
+                        }}
+                        transition={{
+                            repeat: Infinity,
+                            duration: 30,
+                            ease: "linear",
+                        }}
+                        style={{ willChange: "transform" }}
+                    >
+                        {[...Array(6)].map((_, repeatIndex) =>
+                            images.map((img, index) => (
+                                <motion.div
+                                    key={`${repeatIndex}-${index}`}
+                                    className="relative w-[110px] h-[230px] bg-white shadow-lg rounded-xl overflow-hidden sm:w-[140px] sm:h-[250px]"
+                                    whileHover={{ scale: 1.05 }}
+                                >
+                                    <img
+                                        src={img}
+                                        alt={`Image ${index}`}
+                                        className="w-full h-full object-top object-cover"
+                                    />
+                                </motion.div>
+                            ))
+                        )}
+                    </motion.div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Slider;
